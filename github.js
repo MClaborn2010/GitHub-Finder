c4bc9989eaaa168caaf6634a7e31e2be8ea4b48b
Michael Claborn <mclaborn2010@gmail.com>
2019-07-29 05:08:20 -0500

    final build

--- github.js
+++ github.js
@@ -1,17 +1,32 @@
 class Github {
-    constructor (){
-        this.client_id = '22e0a74513cab89df921';
-        this.client_secret = '91020b52ea5d8483045d3e835e822a8c516cae26';
-    }
+  constructor() {
+    this.client_id = "22e0a74513cab89df921";
+    this.client_secret = "d29e6aaef9ddda8fecac8fbaea1ba09a7f66649d";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
+  }
 
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${
+        this.client_id
+      }&client_secret=${this.client_secret}`
+    );
 
-    async getUser(user){
-        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id =${this.client_id}&client_secret =${this.client_secret}`);
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${
+        this.repos_count
+      }&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${
+        this.client_secret
+      }`
+    );
 
-        const profile = await profileResponse.json();
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
-        return {
-            profile
-        }
-    }
+    return {
+      profile,
+      repos
+    };
+  }
 }
